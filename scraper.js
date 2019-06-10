d70c64a1a8e97aee8151baa729ea0df8575ad648
JungHyunKwon <kjh_3859@naver.com>
2019-06-10 18:47:20 +0900

    published

--- scraper.js
+++ scraper.js
@@ -9,9 +9,9 @@
 const fs = require('fs'),
 	  URL = require('url'),
 	  scrape = require('website-scraper'), // {@link https://github.com/website-scraper/node-website-scraper}
-	  phantomHTML = require('website-scraper-phantom'), // {@link https://github.com/website-scraper/node-website-scraper-phantom}
+	  PhantomPlugin = require('website-scraper-phantom'), // {@link https://github.com/website-scraper/node-website-scraper-phantom}
 	  filenamify = require('filenamify'), // {@link https://github.com/sindresorhus/filenamify}
-	  baseDirectory = './dist',
+	  distDirectory = './dist',
 	  userAgent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:61.0) Gecko/20100101 Firefox/61.0',
 	  readline = require('readline'),
 	  rl = readline.createInterface({
@@ -79,77 +79,40 @@ function getName(value) {
 	return name + ' - ' + year + '년 ' + month + '월 ' + day + '일 ' + meridiem + ' ' + hour + '시 ' + minute + '분 ' + second + '초';
 }
 
-/**
- * @param {obejct} options {
-       url : string,
-	   cookie : string,
-	   isDynamic : boolean
-   }
- * @param {function} callback {string}
- */
-function scraper(options, callback) {
-	let callbackIsFunction = typeof callback === 'function',
-		result = '';
-
-	//객체일 때
-	if(options) {
-		let url = options.url,
-			saveDirectory = baseDirectory + '/' + getName(URL.parse(url).hostname),
-			cookie = options.cookie,
-			settings = {
-				urls : url,
-				directory : saveDirectory,
-				updateMissingSources : true,
-				//recursive : true, //재귀
-				//ignoreErrors : false, //오류 무시
-				prettifyUrls : true,
-				request : {
-					headers : {
-						'User-Agent' : userAgent
-					}
-				}
-			};
-
-			//문자일 때
-			if(typeof cookie === 'string') {
-				settings.request.headers.cookie = cookie;
-			}
-			
-			//동적일 때
-			if(options.isDynamic === true) {
-				settings.httpResponseHandler = phantomHTML;
-			}
-
-			scrape(settings, (err, result) => {
-				//오류가 없으면서 저장되었을 때
-				if(!err && result[0].saved) {
-					result = saveDirectory;
-				}
-
-				callback(result);
-			});
-	//함수일 때
-	}else if(callbackIsFunction) {
-		callback(result);
-	}
-}
-
 //질문
 rl.question('주소 : ', url => {
 	//값이 있을 때
 	if(url) {
 		rl.question('쿠키 : ', cookie => {
 			rl.question('동적입니까? ', isDynamic => {
-				scraper({
-					url : url,
-					isDynamic : isDynamic.toLowerCase() === 'true',
-					cookie : (cookie) ? cookie : null
-				}, result => {
-					//저장했을 때
-					if(result) {
-						console.log(result + '에 저장했습니다.');
+				let saveDirectory = distDirectory + '/' + getName(URL.parse(url).hostname),
+					headers = {
+						'User-Agent' : userAgent
+					},
+					settings = {
+						urls : url,
+						directory : saveDirectory,
+						request : {
+							headers : headers
+						}
+					};
+
+				//값이 있을 때
+				if(cookie) {
+					headers.cookie = cookie;
+				}
+
+				//참일 때
+				if(isDynamic === 'true') {
+					settings.httpResponseHandler = PhantomPlugin;
+				}
+
+				scrape(settings, (err, result) => {
+					//오류가 있을 때
+					if(err) {
+						console.error(saveDirectory + '에 저장하지 못했습니다.');
 					}else{
-						console.error(result + '에 저장하지 못했습니다.');
+						console.log(saveDirectory + '에 저장했습니다.');
 					}
 				});
 
